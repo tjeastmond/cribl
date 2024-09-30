3d8eaa4b4b49bd5c9f1a7843de7c63ee3b304d29
TJ Eastmond <tj.eastmond@gmail.com>
2024-09-30 14:04:53 -0400

    Connect to Reader

--- src/controllers/logs.ts
+++ src/controllers/logs.ts
@@ -8,13 +8,14 @@ function logsPath(fileName: string) {
 export async function getLogs(req: Request, res: Response) {
   const fileName = req.body.file_name;
   const keywords = req.body.keywords || [];
+  const numResults = req.body.num_results || 100;
 
   if (!fileName) {
     res.status(400).json({ message: "Missing a valid file name" });
     return;
   }
 
-  const results = await readLogFile(logsPath(fileName as string), 1000, keywords);
+  const results = await readLogFile(logsPath(fileName as string), numResults, keywords);
 
   res.status(200).json({
     numResults: results.length,
