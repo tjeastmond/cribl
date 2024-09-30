1271166e96cd37204de33e151d698378e08a7b3b
TJ Eastmond <tj.eastmond@gmail.com>
2024-09-29 22:47:45 -0400

    Added docs and updated local imports

--- src/app/reader/main.ts
+++ src/app/reader/main.ts
@@ -1,13 +1,23 @@
 import * as fs from "fs";
-import { searchBy } from "./search";
+import { searchBy } from "@reader/search";
 
+/**
+ * Reads data from a file and writes it to a buffer at a specified offset.
+ *
+ * @param {string} filePath - The path to the file to read from.
+ * @param {Buffer} buffer - The buffer to write the data to.
+ * @param {number} [offSet=0] - The offset in the buffer at which to start writing.
+ * @param {number|null} readSize - The number of bytes to read from the file.
+ * @param {number|null} position - The position in the file from which to start reading.
+ * @returns {Promise<void>} A promise that resolves when the read operation is complete.
+ */
 async function readPromise(
   filePath: string,
   buffer: Buffer,
   offSet: number | 0,
   readSize: number | null,
   position: number | null,
-) {
+): Promise<void> {
   const handle = await fs.promises.open(filePath, "r");
   try {
     await handle.read(buffer, offSet, readSize, position);
@@ -16,7 +26,17 @@ async function readPromise(
   }
 }
 
-async function readFileReverse(filePath: string) {
+/**
+ * Reads a file in reverse order, returning chunks of data until the
+ * beginning of the file is reached. The text is returned in chunks and waits for the next
+ * read to be called before reading the next chunk.
+ *
+ * @param {string} filePath - The path to the file to read from.
+ * @returns {Promise<{ read: function, done: function }>} An object containing the
+ *          read function to retrieve the next chunk of data and
+ *          a done function to check if the end of the file has been reached.
+ */
+async function readFileReverse(filePath: string): Promise<{ read: Function; done: Function }> {
   const stats = await fs.promises.stat(filePath);
   const fileSize = stats.size;
   const chunkSize = 1024;
@@ -26,7 +46,7 @@ async function readFileReverse(filePath: string) {
   let content: string = "";
   let lineBuffer: string = "";
 
-  async function read() {
+  async function read(): Promise<string | undefined> {
     if (position <= 0) return;
 
     [content, lineBuffer] = [lineBuffer, ""];
--- src/app/reader/search.ts
+++ src/app/reader/search.ts
@@ -1,10 +1,27 @@
+/**
+ * Defines the allowed types for the search method.
+ * Can be either "array" or "text". Defaults to "text".
+ */
 type SearchByType = "array" | "text";
 const DEFAULT_SEARCHBY: SearchByType = "text";
 
+/**
+ * Checks if any keyword exists within a given string, ignoring case.
+ *
+ * @param {string} line - The string to be checked for keywords.
+ * @param {string[]} keywords - An array of keywords to search for within the line.
+ * @returns {boolean} Returns true if any keyword is found in the line; otherwise, false.
+ */
 export function checkString(line: string, keywords: string[]): boolean {
   return keywords.some((keyword) => line.toLowerCase().includes(keyword.toLowerCase()));
 }
 
+/**
+ * Extracts the last line from a given text and returns it along with the remaining text.
+ *
+ * @param {string} text - The input text to be processed.
+ * @returns {[string, string]} An array containing the last line of the input and the remaining text.
+ */
 export function getLastLine(text: string): [string, string] {
   const trimmedText = text.trimEnd();
   const index = trimmedText.lastIndexOf("\n");
@@ -13,6 +30,14 @@ export function getLastLine(text: string): [string, string] {
   return [lastLine, updatedText];
 }
 
+/**
+ * Returns an array of strings pulled from the input text by searching each line.
+ *
+ * @param {string} text - The text to search through, filtered if keywords are provided.
+ * @param {number} needed - The maximum number of results expected.
+ * @param {string[]} [keywords=[]] - An optional array of keywords to filter the results.
+ * @returns {string[]} An array of strings containing log lines, filtered if keywords are provided.
+ */
 export function searchByText(text: string, needed: number, keywords: string[] = []): string[] {
   const found: string[] = [];
   let remainingText = text;
@@ -29,6 +54,14 @@ export function searchByText(text: string, needed: number, keywords: string[] = 
   return found;
 }
 
+/**
+ * Returns an array of strings pulled from the input text by searching each line.
+ *
+ * @param {string} text - The text to search through. Each line will be checked.
+ * @param {number} needed - The maximum number of results expect.
+ * @param {string[]} [keywords=[]] - An optional array of keywords to filter the results.
+ * @returns {string[]} An array of strings containing log lines, filtered if keywords are provided.
+ */
 export function searchByArray(text: string, needed: number, keywords: string[] = []): string[] {
   const found: string[] = [];
   const lines = text.split("\n");
@@ -43,6 +76,17 @@ export function searchByArray(text: string, needed: number, keywords: string[] =
   return found;
 }
 
+/**
+ * Returns an array of strings pulled from log texts, and may by filtered by keywords.
+ *
+ * @param {string} text - The text to search within.
+ * @param {number} needed - The maximum number of results to return.
+ * @param {string[]} [keywords=[]] - An array of keywords to filter the results.
+ * @param {SearchByType} [searchByValue=DEFAULT_SEARCHBY] - The method to use for log searching.
+ *                                                          Can be either "array" to search by lines
+ *                                                          or "text" to search using line extraction.
+ * @returns {string[]} An array of strings containing log lines, filtered if keywords are provided.
+ */
 export function searchBy(
   text: string,
   needed: number,
