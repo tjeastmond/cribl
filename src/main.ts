76234ec013d85bb2f33a45829a69498e370b7e68
TJ Eastmond <tj.eastmond@gmail.com>
2024-09-30 01:19:47 -0400

    Environment variables

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import express from "express";
 import router from "@routes/router";
 import middleware from "@middleware/connect";
 
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
