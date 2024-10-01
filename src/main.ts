7034b3edd219682229a71ac75b7f5f3399b48f39
TJ Eastmond <tj.eastmond@gmail.com>
2024-09-30 23:45:01 -0400

    Removing dead code

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import express from "express";
 import router from "@routes/router";
 import middleware from "@middleware/connect";
 
-require("dotenv").config();
-
 const app = express();
 const port = process.env.PORT || 3000;
 
